53ee59e93794535eaecf879dc1932c571309709f
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-04-23 00:10:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+});
+function toggleMenu() {
+    mobile_menu.classList.toggle("is-active");
+}
+
